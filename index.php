4d128549f78d04480046721304ca430fab9c53c2
Giuliano Scaglioni <scaglioni96@gmail.com>
2023-08-25 10:28:50 -0300

    agregue h2 y algo al index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1024px, initial-scale=1.0">
+    <title>un php </title>
+</head>
+<body>
+    
+</body>
+</html>
